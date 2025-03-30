192e44a72274a981eda84c73c9aa31d869f71a82
thomaz sachetto <thomazsachetto@gmail.com>
2025-03-30 08:27:07 -0300

    Create change_att.ps1

---
+++ change_att.ps1
@@ -0,0 +1,3 @@
+Win-Powershell:
+Set-ItemProperty -Path "C:\filepath\filename.ext" -Name CreationTime -Value "01 March 2020 07:10:02"
+Set-ItemProperty -Path "C:\filepath\filename.ext" -Name LastWriteTime -Value "01 March 2020 09:49:35"
